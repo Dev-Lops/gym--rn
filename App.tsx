86fb4c858ed9d4adc5bc890dacb8f6913c7538e9
Anderson Lopes <developes.anderson@gmail.com>
2024-07-31 22:28:44 -0400

    ✨ Configurando o loading de font

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flex: 1, justifyContent: 'center', alignItems: 'center',
       }}>
-      <Text>Home</Text>
+      {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
   );
 }
