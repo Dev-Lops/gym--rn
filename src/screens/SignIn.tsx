24f20cab04721683c146dd6686a1cae27b745864
Anderson Lopes <developes.anderson@gmail.com>
2024-07-31 23:20:18 -0400

    ✨ Typando o Svg

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,11 @@
-import { Image, VStack } from "@gluestack-ui/themed";
+import { Center, Image, Text, VStack } from "@gluestack-ui/themed";
 
 import BackGroundImg from "@assets/background.png"
+import Logo from "@assets/logo.svg"
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="$gray700">
+    <VStack flex={1} bg="$backgroundDark400">
       <Image
         w="$full"
         h={624}
@@ -13,6 +14,14 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
+
+      <Center my={'$24'}>
+        <Logo size={128} />
+
+        <Text color="$gray100" fontSize={"$sm"}>
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
 
     </VStack>
   )
