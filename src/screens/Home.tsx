00bf7340d396be1b04b23daed8bf0022e4e84ec0
Anderson Lopes <developes.anderson@gmail.com>
2024-08-01 15:25:14 -0400

    ✨ Lista de exercicios e header dos exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,47 @@
+/* eslint-disable react-native/no-raw-text */
+/* eslint-disable react-native/no-inline-styles */
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from '@gluestack-ui/themed';
+import { FlatList, Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
+import { useState } from 'react';
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    'Costas',
+    'Bíceps',
+    'Tríceps',
+    'Ombro',
+  ]);
+  const [groupSelected, setGroupSelected] = useState('Costas');
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => (
+          <Group
+            name={item as string}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item as string)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+      <VStack px="$8">
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercicios
+          </Heading>
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
