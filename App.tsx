157ad5853a543847191150a0adc88c86c3d90b68
Anderson Lopes <developes.anderson@gmail.com>
2024-07-31 22:56:25 -0400

    ✨ Customizando o tema padrão

--- App.tsx
+++ App.tsx
@@ -1,28 +1,29 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 import { StatusBar, View } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
   return (
-    <GluestackUIProvider>
-      <View
-        style={{
-          flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202024'
-        }}
-      >
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
+    <GluestackUIProvider config={config}>
+
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Center flex={1} bg='$info600'>
+          <Text>
+            Home
+          </Text>
+        </Center>
+      ) : (
+        <View
         />
-        {fontsLoaded ? <Text>
-          Home
-        </Text>
-          :
-          <View
-          />}
-      </View>
+      )}
+
     </GluestackUIProvider>
   );
 }
