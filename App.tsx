879b7fa0bc4bf369c493f50e5fe0fad669358876
Anderson Lopes <developes.anderson@gmail.com>
2024-07-31 22:31:23 -0400

    ✨ Configurando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
   return (
     <View
       style={{
-        flex: 1, justifyContent: 'center', alignItems: 'center',
-      }}>
+        flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202024'
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
   );
